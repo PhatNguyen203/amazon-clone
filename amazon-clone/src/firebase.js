7d69f404771e4707d1dac68038cb1f909fab90b5
PhatNguyen203 <Lee0ne203@gmail.com>
2020-10-11 22:52:16 -0400

    initialize react app and firebase config

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAc6jgEVFZu3SOBUNplVE-_YWKFDMYyhOo",
+  authDomain: "clone-98a01.firebaseapp.com",
+  databaseURL: "https://clone-98a01.firebaseio.com",
+  projectId: "clone-98a01",
+  storageBucket: "clone-98a01.appspot.com",
+  messagingSenderId: "974903600202",
+  appId: "1:974903600202:web:e140440490e596584a32b3",
+  measurementId: "G-8FQDPWNNLX",
+};
