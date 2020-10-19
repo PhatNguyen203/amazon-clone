2d7ac43a16ede8cd9881fda482f05ceadcd840fc
PhatNguyen203 <Lee0ne203@gmail.com>
2020-10-19 15:48:22 -0500

    added Checkout component

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
         <div className="home_container">
           <img
             className="home_image"
-            src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE_XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+            src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
             alt="picture cover"
           />
   
---
+++ amazon-clone/src/Subtotal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './subtotal.css';
+
+const Subtotal = () => {
+    return (
+        <div className="subtotal">
+            <h2>Subtotal Section</h2>
+        </div>
+    )
+}
+
+export default Subtotal;
