335b0a6e68fd8b5679b72bb572fe56c254bc2722
PhatNguyen203 <Lee0ne203@gmail.com>
2020-10-19 22:10:19 -0500

    added Subtotal component

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -6,15 +6,14 @@ const Checkout = () => {
     return(
         <div className="checkout">
             <div className="checkout_left">
-            <img
-                className="checkout_ad"
-                src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
-                alt=""
-            />
-            </div>
-            <div>
-                <h1>Hello, user</h1>
-                <h2 className="checkout_title">Your shopping Basket</h2>
+                <img
+                    className="checkout_ad"
+                    src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg"
+                    alt=""
+                />
+                <div>
+                    <h2 className="checkout_title">Your shopping Basket</h2>
+                </div>
             </div>
             <div className="checkout_right">
                 <Subtotal />
--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import './subtotal.css';
 
+import CurrencyFormat from 'react-currency-format';
+
 const Subtotal = () => {
     return (
-        <div className="subtotal">
-            <h2>Subtotal Section</h2>
-        </div>
+       <div className="subtotal">
+            <CurrencyFormat
+                renderText={(value) => (
+                <>
+                <p>
+                {/* Part of the homework */}
+                Subtotal (0 items): <strong>0</strong>
+                </p>
+                <small className="subtotal_gift">
+                <input type="checkbox" /> This order contains a gift
+                </small>
+            </>
+            )}
+                decimalScale={2}
+                value={0} // Part of the homework
+                displayType={"text"}
+                thousandSeparator={true}
+                prefix={"$"}
+        />
+        <button>Process your checkout</button>
+       </div>
     )
 }
 
