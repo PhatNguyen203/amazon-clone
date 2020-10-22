338f7e45334159f50f4d275cc98647bc02186280
PhatNguyen203 <Lee0ne203@gmail.com>
2020-10-21 23:52:33 -0500

    added subtotal and get basket total

--- amazon-clone/src/reducer.js
+++ amazon-clone/src/reducer.js
@@ -2,6 +2,12 @@ export const initialState = {
     basket:[]
 }
 
+//selector
+export const getBasketTotal = (basket) =>{ 
+    console.log(basket);
+    return basket?.reduce((amount, item)=> item.price + amount,0)
+}
+
 const reducer = (state, action) => {
     switch (action.type) {
         case "ADD_TO_BASKET":
