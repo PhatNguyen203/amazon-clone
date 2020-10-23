89464c93c0d19eafa68b732d0a0503a4b73bec26
PhatNguyen203 <Lee0ne203@gmail.com>
2020-10-22 22:59:33 -0500

    worked with CheckoutProduct component

--- amazon-clone/src/CheckoutProduct.js
+++ amazon-clone/src/CheckoutProduct.js
@@ -3,10 +3,27 @@ import './checkout_product.css';
 
 import {useStateValue} from './StateProvider';
 
-const CheckoutProduct = ({id,price,title,rating}) => {
+const CheckoutProduct = ({id,price,title,rating,image}) => {
     return(
         <div className='checkoutProduct'>
-            Test Product
+            <img src={image} alt='' className='checkoutProduct__image'/>
+            <div className='checkoutProduct__info'>
+                <p className='checkout__title'>{title}</p>
+                <p className='checkout__price'>
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className='checkout__rating'>
+                    {
+                        Array(rating)
+                        .fill()
+                        .map((_,i) => (
+                            <p key={i}>🌟</p>
+                        ))
+                    }
+                </div>
+            </div>
+            <button>Remove from Basket</button>
         </div>
     )
 }
