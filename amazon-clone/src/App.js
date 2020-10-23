f8a3c6754292a2f26ab1fa249089c54aa74d4d1e
PhatNguyen203 <Lee0ne203@gmail.com>
2020-10-23 00:16:12 -0500

    working with front-end login page

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import './App.css';
-import Header from './Header'
-import Home from './Home'
-import Checkout from './Checkout'
+import Header from './Header';
+import Home from './Home';
+import Checkout from './Checkout';
+import Login from './Login';
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <Header />
         <Switch>
           <Route exact path="/">
+            <Header />
             <Home />
           </Route>
           <Route exact path="/checkout">
+            <Header />
             <Checkout />
+          </Route>
+          <Route exact path='/login'>
+            <Login />
           </Route>
         </Switch>
       </div>
--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -14,46 +14,47 @@ const Header = () => {
     return(
         <div className="header">
             <Link to="/">
-                <img src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo" className="header_logo"/>
+                <img src="http://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo" className="header__logo"/>
             </Link>
-            <div className="header_search">
-                <input className="header_searchInput" type="text" />
-                <SearchIcon className="header_searchIcon" />
+            <div className="header__search">
+                <input className="header__searchInput" type="text" />
+                <SearchIcon className="header__searchIcon" />
             </div>
-            <div className="header_nav">
-                <div className="header_option">
-                    <span className="header_option_lineOne">
-                        Hello, Signin
-                    </span>
-                    <span className="header_option_lineTwo">
-                        Account & List
-                    </span>
-                </div>
-                <div className="header_option">
-                <span className="header_option_lineOne">
+            <div className="header__nav">
+                    <Link to='/login'>
+                        <div className="header__option">
+                            <span className="header__optionLineOne">
+                                Hello, Guest
+                            </span>
+                            <span className="header__optionLineTwo">
+                                Signin
+                            </span>
+                        </div>
+                    </Link>
+                <div className="header__option">
+                <span className="header__optionLineOne">
                         Returns
                     </span>
-                    <span className="header_option_lineTwo">
+                    <span className="header__optionLineTwo">
                         & Orders
                     </span>
                 </div>
-                <div className="header_option">
-                <span className="header_option_lineOne">
+                <div className="header__option">
+                <span className="header__optionLineOne">
                         Your
                     </span>
-                <span className="header_option_lineTwo">
+                <span className="header__optionLineTwo">
                         Prime
                     </span>
                 </div>
                 <Link to="/checkout">
-                    <div className="header_optionBasket">
+                    <div className="header__optionBasket">
                         <ShoppingBasketIcon />
-                        <span className="header_option_lineTwo header_basketCount">
+                        <span className="header__option_lineTwo header__basketCount">
                         {basket?.length}    
                         </span>  
                     </div>
                 </Link>
-               
             </div>
         </div>
     )
