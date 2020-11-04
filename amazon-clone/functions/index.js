ead8038aa20037097eac3f92f8a4e22f70faeb10
PhatNguyen203 <Lee0ne203@gmail.com>
2020-11-03 22:32:57 -0600

    created back-end payment functionality

--- amazon-clone/functions/index.js
+++ amazon-clone/functions/index.js
@@ -1,9 +1,31 @@
 const functions = require('firebase-functions');
+const express =require('express');
+const cors = require('cors');
+const stripe = require('stripe')(
+    "sk_test_51Hh6RVLM09v9FY07DJvss8B63s1MKxZOAIx2ceNYFYV6CQ4r0PLBRgG3h5qwH0ekgMCBLPBUeNIlZiCFOIEdfG7D00dKUPi17e"
+);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//app config
+const app = express();
+//Middleware
+app.use(cors({origin:true}))
+app.use(express.json())
+
+// - API routes
+app.get("/", async (request, response) => response.status(200).send("hello world"));
+
+app.post("/payments/create",async (request, response) => {
+    const total = request.query.total
+    console.log("total is "+ total)
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: total, //subunit
+        currency: "usd"
+    })
+
+    response.status(201).send({
+        clientSecret = paymentIntent.client_secret
+    })
+})
+
+// - Listen command
+exports.api = functions.https.onRequest(app);
